9a63473cbb80f07cf5013cb79a5649be16db2450
DESKTOP-9CKSS1Q\CAMT <[nuntachat_pat@cmu.ac.th]>
2017-03-20 17:55:41 +0700

    Change User Class

--- src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
@@ -8,16 +8,17 @@ import java.util.List;
 
 public class UserDaoImpl {
     List<User> users;
+
     public UserDaoImpl() {
         users = new ArrayList<>();
         users.add(new User("Prayuth","1234","Tu",
                 LocalDate.of(1979,2,14),"08612345678"));
         users.add(new User("Tucky","5675","Tuckung",
                 LocalDate.of(1999,8,30),"08687654321"));
-        users.add(User.builder().name("Honey").username("Honey").password("aabbcc")
-                .dateOfBirth(LocalDate.of(2012,11,13)).telephoneNo("0000000000").build());
-        users.add(User.builder().name("NoName").username("None").password("none")
-                .dateOfBirth(LocalDate.of(2112,1,1)).telephoneNo("9999999999").build());
+        users.add(new User("Honey", "aabbcc", "Honey",
+                        LocalDate.of(2012,11,13), "0000000000"));
+        users.add(new User("none", "None", "NoName",
+                LocalDate.of(2112,1,1), "9999999999"));
     }
 
     public List<User> getUsers(){
