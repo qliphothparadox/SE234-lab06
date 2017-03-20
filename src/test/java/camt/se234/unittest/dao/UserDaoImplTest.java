9f56e40b9f12140b3cdd887e4c81e07551c73544
Nattita Boonma <nattita_b@cmu.ac.th>
2017-03-20 17:26:20 +0700

    Add contains test

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 
 import java.time.LocalDate;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 
@@ -21,6 +22,16 @@ public class UserDaoImplTest {
                         new User("Tucky","5675","Tuckung",
                                 LocalDate.of(2014,11,22),"08687654321")
                 ));
+        assertThat(userDao.getUsers(),
+                contains(new User("Prayuth","1234","Tu",
+                                LocalDate.of(2016,12,28),"08612345678"),
+                        new User("Tucky","5675","Tuckung",
+                                LocalDate.of(2014,11,22),"08687654321"),
+                        new User("Honey","aabbcc", "Honey",
+                                LocalDate.of(2012,11,13), "0000000000" ),
+                        new User("None", "none", "NoName",
+                                LocalDate.of(2112, 1,1), "9999999999")));
+
     }
 
 
