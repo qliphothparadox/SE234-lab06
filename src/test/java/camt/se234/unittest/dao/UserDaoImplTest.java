3114300185f7843ec01330f672f18e656907c1e2
Phornthep <phornthep_chooleat@cmu.ac.th>
2017-03-20 17:51:13 +0700

    Add test

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -9,6 +9,7 @@ import org.junit.rules.ExpectedException;
 import java.time.LocalDate;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 
@@ -31,13 +32,16 @@ public class UserDaoImplTest {
                                 LocalDate.of(2012,11,13), "0000000000" ),
                         new User("None", "none", "NoName",
                                 LocalDate.of(2112, 1,1), "9999999999")));
+        assertThat(userDao.getUsers(),
+                containsInAnyOrder(
+                        new User("NoName","none","None",LocalDate.of(2112,01,01),"9999999999"),
+                        new User("Tucky","5675","Tuckung", LocalDate.of(2014,11,22),"08687654321"),
+                        new User("Prayuth","1234","Tu", LocalDate.of(2016,12,28),"08612345678"),
+                        new User("Honey", "aabbcc", "Honey", LocalDate.of(2012,11,13), "0000000000")
+                )
 
+        );
     }
-
-    /**
-     * asdfjasldfas
-     * fdsafsd
-     */
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -51,7 +55,6 @@ public class UserDaoImplTest {
         thrown.expect(NullPointerException.class);
         userService.login("","");
     }
-
 
 
 }
